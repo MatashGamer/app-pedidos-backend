ce4741585ed881eb349ec79cb1ba0891c3ae8e7e
MatashGamer <111697037+MatashGamer@users.noreply.github.com>
2022-10-12 11:55:51 -0500

    version 1.0.0 de la aplicacion de pedidos

---
+++ src/models/pedido.model.ts
@@ -0,0 +1,58 @@
+import {Entity, model, property, belongsTo, hasOne} from '@loopback/repository';
+import {Persona} from './persona.model';
+import {Producto} from './producto.model';
+
+@model()
+export class Pedido extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  IdProducto: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Total: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Estado: number;
+
+  @belongsTo(() => Persona)
+  PersonaId: string;
+
+  @property({
+    type: 'string',
+  })
+  personaId?: string;
+
+  @hasOne(() => Producto)
+  producto: Producto;
+
+  constructor(data?: Partial<Pedido>) {
+    super(data);
+  }
+}
+
+export interface PedidoRelations {
+  // describe navigational properties here
+}
+
+export type PedidoWithRelations = Pedido & PedidoRelations;
